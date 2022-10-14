b6077f6f0e6a8fa5b4a59fd8efa7e9a61ccbb823
Jonathan Carballo <jonathanfrancisco.carballo@unab.edu.sv>
2022-10-14 08:58:00 -0600

    20221013

--- app/src/main/java/com/example/sqliteapp2/MainActivity.kt
+++ app/src/main/java/com/example/sqliteapp2/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sqliteapp2
 
 import android.content.ContentValues
+import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -18,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         etnCodigo = findViewById(R.id.etn_Codigo)
-        etDescripcion = findViewById(R.id.etn_Codigo)
-        etnPrecio = findViewById(R.id.etn_Codigo)
+        etDescripcion = findViewById(R.id.et_Descripcion)
+        etnPrecio = findViewById(R.id.etn_Precio)
     }
 
     fun registrar(vista: View){
@@ -32,8 +33,8 @@ class MainActivity : AppCompatActivity() {
         val precio = etnPrecio.text.toString()
 
         if(!codigo.isEmpty() && !descripcion.isEmpty() && !precio.isEmpty()){
-            codigo.toInt()
-            precio.toDouble()
+            //codigo.toInt()
+            //precio.toDouble()
             val registro = ContentValues()
             registro.put("codigo",codigo)
             registro.put("descripcion",descripcion)
@@ -50,4 +51,104 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,"Debes llenar todos los campos",Toast.LENGTH_SHORT).show()
         }
     }
+
+    fun buscar(vista:View){
+        //se crea la conexion a las base de datos
+        val admin = AdminSQLite(this,"Tienda",null,1)
+        val baseDeDatos:SQLiteDatabase = admin.writableDatabase
+
+        val codigo =etnCodigo.text.toString()
+
+        if(!codigo.isEmpty()){
+            //verificar si esta el codigo del produc
+            val fila = baseDeDatos.rawQuery("select descripcion, precio from articulos where codigo=${codigo}",null)
+            if(fila.moveToFirst()){
+                etDescripcion.setText(fila.getString(0))
+                etnPrecio.setText(fila.getString(1))
+                baseDeDatos.close()
+            }else{
+                Toast.makeText(this,"No existe el articulo", Toast.LENGTH_SHORT).show()
+                baseDeDatos.close()
+            }
+        }else{
+            Toast.makeText(this,"Debes ingresar un codigo", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    fun modificar(vista: View){
+        //se crea la conexion a las base de datos
+        val admin = AdminSQLite(this,"Tienda",null,1)
+        val baseDeDatos:SQLiteDatabase = admin.writableDatabase
+
+        val codigo = etnCodigo.text.toString()
+        val descripcion = etDescripcion.text.toString()
+        val precio = etnPrecio.text.toString()
+
+        if(!codigo.isEmpty() && !descripcion.isEmpty() && !precio.isEmpty()){
+            //codigo.toInt()
+            //precio.toDouble()
+            val registro = ContentValues()
+            registro.put("codigo",codigo)
+            registro.put("descripcion",descripcion)
+            registro.put("precio",precio)
+
+            val cantidad:Int = baseDeDatos.update("articulos",registro,"codigo=${codigo}",null)
+            baseDeDatos.close()
+
+            //verifica si la consulta tiene valor
+
+            if(cantidad==1){
+                Toast.makeText(this,"Articulo modificado correctamente",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,"No existe el articulo",Toast.LENGTH_SHORT).show()
+                baseDeDatos.close()
+            }
+
+            etnCodigo.setText("")
+            etDescripcion.setText("")
+            etnPrecio.setText("")
+
+            Toast.makeText(this,"Registro exitoso",Toast.LENGTH_LONG).show()
+        }else{
+            Toast.makeText(this,"Debes llenar todos los campos",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    fun eliminar(vista: View){
+        //se crea la conexion a las base de datos
+        val admin = AdminSQLite(this,"Tienda",null,1)
+        val baseDeDatos:SQLiteDatabase = admin.writableDatabase
+
+        val codigo =etnCodigo.text.toString()
+
+        if(!codigo.isEmpty()){
+            //verificar si esta el codigo del produc
+                val cantidad:Int = baseDeDatos.delete("articulos","codigo=${codigo}",null)
+                baseDeDatos.close()
+                etnCodigo.setText("")
+                etDescripcion.setText("")
+                etnPrecio.setText("")
+
+                if(cantidad==1){
+                    Toast.makeText(this,"Articulo eliminado",Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this,"No existe el articulo",Toast.LENGTH_SHORT).show()
+                    baseDeDatos.close()
+                }
+
+            }else{
+            Toast.makeText(this,"Debes ingresar un codigo", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun siguiente(vista: View){
+        val ventana:Intent = Intent(applicationContext,SharedPerferencesApp::class.java)
+        startActivity(ventana)
+    }
+
+
+
+
 }
